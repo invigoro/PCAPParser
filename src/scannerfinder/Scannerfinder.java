6add3cc2da6a073d146c5629b8b73188831536ae
Timothy Wells <35319035+invigoro@users.noreply.github.com>
2019-03-26 11:18:06 -0600

    Final

--- src/scannerfinder/Scannerfinder.java
+++ src/scannerfinder/Scannerfinder.java
@@ -14,33 +14,40 @@ import org.jnetpcap.util.PcapPacketArrayList;
 
 /**
  *
- * @author Tim Wells
+ * @author Tim Wells and Cory Peterson
  */
 public class Scannerfinder {
 
     /**
-     * @param args the command line arguments
+     * @param arg[0] is the filepath to the pcap file
      */
-    final static Ip4 ip = new Ip4();
-    public static Hashtable<String, int[]> hash = new Hashtable<String, int[]>();
-    public static int count = 0;
+    
+    
+    final static Ip4 ip = new Ip4(); //used for retreiving IP addresses
+    public static Hashtable<String, int[]> hash = new Hashtable<String, int[]>();   //Stores IP address as key, and int[2] array where [0] is number of sent packets and [1] is number received
+    public static int count = 0; //number of packets parsed
 
     public static void main(String[] args) throws IOException {
-        // TODO code application logic here
+        // filepath of pcap file
         String file = args[0];
+        
+        //open and read the packet
         StringBuilder errbuf = new StringBuilder();
         Pcap pcap = Pcap.openOffline(file, errbuf);
         if (pcap == null) {
             throw new IOException(errbuf.toString());
         }
         pcap.loop(-1, pcappackethandler, file);
+        
+        //Print results
         System.out.println("\n" + count + " packets examined. Suspicious IP addresses:\n------------------------------------------------------------------------------------------------");
+        //Iterate through the hashtable 
         Enumeration<String> keys = hash.keys();
         while (keys.hasMoreElements()) {
             String key = keys.nextElement();
             int[] vals = hash.get(key);
-            float ratio = 0;
-            if (vals[1] > 0) {
+            float ratio = 0;    //ratio of sent to received
+            if (vals[1] > 0) {  //so we don't accidentally divide by zero. Oops
                 ratio = (float) ((float) vals[0] / (float) vals[1]);
             }
             if (ratio > 3) {
@@ -61,6 +68,8 @@ public class Scannerfinder {
                 //System.out.println("IP type:\t" + ip.typeEnum());
                 //System.out.println("IP src:\t-\t" + source);
                 //System.out.println("IP dst:\t-\t" + dest);
+                
+                //add to number of sent/received for ip in hashtable
                 if (hash.containsKey(source)) {
                     int[] val = hash.get(source);
                     val[0]++;
